0b2019e698593a50deff349f73a038f971519c7e
Denuwan-md <denuwanmd@gmail.com>
2025-02-13 22:52:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=your_paircode",
+SESSION_ID: process.env.SESSION_ID || "mDBxXZ7S#UQD_hKwh0u7jk0sCL0c6bVBG5bouOFcMGqtdj5XIcTg",
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
